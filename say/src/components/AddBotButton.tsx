608fe460fcf582e504aabe02453adf773ed2cef6
Mike Moldawsky <mike.moldawsky@gmail.com>
2023-03-25 16:49:25 +0300

    initial design

--- say/src/components/AddBotButton.tsx
+++ say/src/components/AddBotButton.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
 
-const AddBotButton :  React.FC = () => {
+const AddBotButton: React.FC = () => {
 	return (
 		<Link href="/configure-bot">
-				<button>Add Bot</button>
+			<button className="bg-purple-500 text-white px-6 py-2 rounded-md mt-4">Add Bot</button>
 		</Link>
 	);
 };
--- say/src/components/BotCard.tsx
+++ say/src/components/BotCard.tsx
@@ -11,20 +11,20 @@ interface BotCardProps {
 	bot: Bot;
 }
 
-const BotCard : React.FC<BotCardProps> = ({ bot }) => {
+const BotCard: React.FC<BotCardProps> = ({ bot }) => {
 	return (
-		<div>
-			<h3>{bot.name}</h3>
-			<p>{bot.behavior}</p>
-			<Link href={`/configure-bot?id=${bot.id}`}>
-
-				<button>Configure</button>
-			</Link>
-			<Link href={`/chat?id=${bot.id}`}>
-					<button>Chat</button>
-
-			</Link>
-			<button>Delete</button>
+		<div className="bg-white p-4 rounded-lg shadow-md w-full sm:w-72 m-4">
+			<h3 className="font-bold text-xl mb-2">{bot.name}</h3>
+			<p className="text-gray-700 mb-4">{bot.behavior}</p>
+			<div className="flex justify-between">
+				<Link href={`/configure-bot?id=${bot.id}`}>
+					<button className="bg-blue-500 text-white px-4 py-2 rounded-md">Configure</button>
+				</Link>
+				<Link href={`/chat?id=${bot.id}`}>
+					<button className="bg-green-500 text-white px-4 py-2 rounded-md">Chat</button>
+				</Link>
+				<button className="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+			</div>
 		</div>
 	);
 };
