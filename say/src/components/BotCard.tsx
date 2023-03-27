747008baf228d0bd596df91c114d87021d083d09
Mike Moldawsky <mike.moldawsky@gmail.com>
2023-03-27 09:30:00 +0300

    Bots side by side

--- say/src/components/BotCard.tsx
+++ say/src/components/BotCard.tsx
@@ -8,20 +8,20 @@ interface BotCardProps {
 
 const BotCard: React.FC<BotCardProps> = ({ bot }) => {
 	return (
-		<div className="bg-white p-6 rounded-lg shadow-md text-center hover:shadow-lg hover:scale-105 transition-all duration-300 mb-4 flex items-center">
-			<img className="w-40 h-40 object-cover rounded-full mr-8" src={bot.imageUrl} alt={bot.name} />
+		<div className="bg-white p-4 rounded-lg shadow-md text-center hover:shadow-lg hover:scale-105 transition-all duration-300 mb-4 flex items-center">
+			<img className="w-20 h-20 object-cover rounded-full mr-4" src={bot.imageUrl} alt={bot.name} />
 			<div>
-				<h3 className="text-3xl font-bold mb-4">{bot.name}</h3>
-				<p className="text-lg mb-4">{bot.description}</p>
-				<p className="text-base mb-6 italic">{bot.behavior}</p>
-				<div className="flex justify-center space-x-2 mb-6">
+				<h3 className="text-xl font-bold mb-2">{bot.name}</h3>
+				<p className="text-sm mb-2">{bot.description}</p>
+				<p className="text-xs mb-4 italic">{bot.behavior}</p>
+				<div className="flex justify-center space-x-2 mb-4">
 					<Link href={`/configure-bot?id=${bot.id}`}>
-						<button className="bg-blue-500 text-white px-4 py-2 rounded shadow mr-2 hover:bg-blue-600 transition-all duration-300">Configure</button>
+						<button className="bg-blue-500 text-white px-2 py-1 rounded shadow mr-1 hover:bg-blue-600 transition-all duration-300">Configure</button>
 					</Link>
 					<Link href={`/chat?id=${bot.id}`}>
-						<button className="bg-green-500 text-white px-4 py-2 rounded shadow mr-2 hover:bg-green-600 transition-all duration-300">Chat</button>
+						<button className="bg-green-500 text-white px-2 py-1 rounded shadow mr-1 hover:bg-green-600 transition-all duration-300">Chat</button>
 					</Link>
-					<button className="bg-red-500 text-white px-4 py-2 rounded shadow hover:bg-red-600 transition-all duration-300">Delete</button>
+					<button className="bg-red-500 text-white px-2 py-1 rounded shadow hover:bg-red-600 transition-all duration-300">Delete</button>
 				</div>
 			</div>
 		</div>
--- say/src/components/BotList.tsx
+++ say/src/components/BotList.tsx
@@ -32,7 +32,7 @@ const BotList: React.FC<BotListProps> = ({ bots, setBots }) => {
 	return (
 		<div className="p-4">
 			<h2 className="text-2xl font-semibold mb-4 text-primary">My Bots</h2>
-			<div className="flex flex-col items-center space-y-6">
+			<div className="flex flex-wrap justify-center items-center gap-6">
 				{bots.map((bot) => (
 					<BotCard key={bot.id} bot={bot} />
 				))}
