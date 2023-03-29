4de20cf2c4d68d66fbdfa169d3fc0d9a67ce35d6
Mike Moldawsky <mike.moldawsky@gmail.com>
2023-03-29 09:28:11 +0300

    not bad design

--- say/src/components/chat/BotChatContextCard.tsx
+++ say/src/components/chat/BotChatContextCard.tsx
@@ -1,27 +1,60 @@
+// BotChatContextCard.tsx
 import React from 'react';
+import AddContextModal from './AddContextModal';
 
 interface BotChatContextCardProps {
+	index: number;
 	title: string;
-	message: string;
 	isSelected: boolean;
-	onSwitch: () => void;
+	onSwitchContext: (index: number) => void;
+	onEditContext: (index: number) => void;
 }
 
-const BotChatContextCard: React.FC<BotChatContextCardProps> = ({ title, message, isSelected, onSwitch }) => {
+const BotChatContextCard: React.FC<BotChatContextCardProps> = ({
+																   index,
+																   title,
+																   isSelected,
+																   onSwitchContext,
+																   onEditContext,
+															   }) => {
+	const [showEditModal, setShowEditModal] = React.useState(false);
+
+	const handleEditContext = () => {
+		setShowEditModal(true);
+		onEditContext(index);
+	};
+
 	return (
 		<div
-			className={`p-4 bg-white rounded-lg shadow mb-4 border-2 ${
-				isSelected ? 'border-blue-500' : 'border-transparent'
+			className={`p-4 rounded-lg border-2 ${
+				isSelected ? 'border-blue-500' : 'border-gray-200'
 			}`}
 		>
-			<h4 className="font-semibold mb-2">{title}</h4>
-			<p className="text-sm mb-4">{message}</p>
-			<button
-				onClick={onSwitch}
-				className="bg-blue-500 text-white px-5 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
-			>
-				Switch
-			</button>
+			{showEditModal && (
+				<AddContextModal onClose={() => setShowEditModal(false)} />
+			)}
+			<div className="flex items-center justify-between">
+				<h4 className="font-semibold">{title}</h4>
+				<div className="flex items-center space-x-2">
+					<button
+						disabled={isSelected}
+						onClick={() => onSwitchContext(index)}
+						className={`px-2 py-1 rounded-md ${
+							isSelected
+								? 'bg-gray-300 text-gray-500 cursor-not-allowed'
+								: 'bg-blue-500 text-white hover:bg-blue-600'
+						}`}
+					>
+						Switch
+					</button>
+					<button
+						onClick={handleEditContext}
+						className="px-2 py-1 rounded-md bg-yellow-500 text-white hover:bg-yellow-600"
+					>
+						Edit
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
--- say/src/components/chat/BotChatInfo.tsx
+++ say/src/components/chat/BotChatInfo.tsx
@@ -18,10 +18,21 @@ const BotChatInfo: React.FC<BotChatInfoProps> = ({ bot, contexts }) => {
 		setSelectedContext(index);
 	}
 
+	const handleEditContext = (index: number) => {
+		alert("Not implemented yet");
+	}
+
+
 	return (
 		<div className="h-screen overflow-auto p-4 flex flex-col">
 			<div className="flex-grow flex flex-col items-center">
-				<Image className="w-20 h-20 object-cover rounded-full mb-4" src={bot.imageUrl} alt={bot.name} width={80} height={80} />
+				<Image
+					className="w-20 h-20 object-cover rounded-full mb-4"
+					src={bot.imageUrl}
+					alt={bot.name}
+					width={80}
+					height={80}
+				/>
 				<h3 className="text-xl font-bold mb-2">{bot.name}</h3>
 				<p className="text-base mb-2">{bot.description}</p>
 			</div>
@@ -31,18 +42,19 @@ const BotChatInfo: React.FC<BotChatInfoProps> = ({ bot, contexts }) => {
 			>
 				Add Context
 			</button>
-			<div className="space-y-4">
+			<div className="w-full mt-4">
 				{contexts.map((context, index) => (
 					<BotChatContextCard
 						key={index}
 						index={index}
 						title={context.name}
-						message={context.message}
 						isSelected={selectedContext === index}
-						onSwitch={handleSwitchContext}
+						onSwitchContext={handleSwitchContext}
+						onEditContext={handleEditContext}
 					/>
 				))}
 			</div>
+
 			<div className="mt-4 bg-gray-200 rounded-lg px-4 py-2 mb-4">
 				<p className="font-semibold">System Message:</p>
 				<p className="">{bot.systemMessage}</p>
