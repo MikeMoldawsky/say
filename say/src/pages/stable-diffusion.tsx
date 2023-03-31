c94ab00052d7e579c7324c6e186ff5a0886bfde2
Mike Moldawsky <mike.moldawsky@gmail.com>
2023-03-31 08:53:27 +0300

    test

--- say/src/pages/stable-diffusion.tsx
+++ say/src/pages/stable-diffusion.tsx
@@ -16,9 +16,10 @@ const GenerateImage: React.FC = () => {
     const imageRef = useRef<HTMLImageElement | null>(null);
 
     const downloadImage = () => {
-        if (imageRef.current && imageRef.current.src) {
+        const src = imageRef.current?.src ?? null;
+        if (src) {
             const link = document.createElement("a");
-            link.href = imageRef.current.src;
+            link.href = src;
             link.download = "generated-image.png";
             link.click();
         }
@@ -58,34 +59,37 @@ const GenerateImage: React.FC = () => {
 
     return (
         <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-7xl h-[90vh] flex flex-col">
+            <div className="kuku0 bg-white p-8 rounded-lg shadow-md w-full max-w-7xl h-[90vh] flex flex-col w-full h-full">
                 <h1 className="text-4xl font-bold mb-4">Image Generator</h1>
                 <div className="flex flex-grow">
-                    <div className="w-1/3 pr-8 flex flex-col space-y-4">
-                              <textarea
-                                  id="prompt"
-                                  value={userPrompt}
-                                  onChange={(e) => setUserPrompt(e.target.value)}
-                                  className="mt-1 block w-full h-[60vh] p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                              />
-                        <div className="flex-grow flex flex-col">
-                            <button
-                                onClick={fetchImage}
-                                className="w-full px-4 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded mt-4"
-                            >
-                                <FontAwesomeIcon icon={faRedo} className="mr-2" />
-                                Generate Image
-                            </button>
+                    <div className="kuku1 w-1/3 pr-8 flex flex-col space-y-4">
+                        <div className="w-full h-full">
+                             <textarea
+                                 id="prompt"
+                                 value={userPrompt}
+                                 onChange={(e) => setUserPrompt(e.target.value)}
+                                 className="mt-1 block w-full h-[60vh] p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 resize-none"
+                             />
+                            <div className="flex-grow flex flex-col">
+                                <button
+                                    onClick={fetchImage}
+                                    className="w-full px-4 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded mt-4"
+                                >
+                                    <FontAwesomeIcon icon={faRedo} className="mr-2" />
+                                    Generate Image
+                                </button>
+                            </div>
                         </div>
+
                     </div>
-                    <div className="relative w-2/3 border-l border-gray-300 pl-8">
+                    <div className="kuku2 relative w-2/3 border-gray-300 pl-8">
                         <div className="absolute top-0 left-0 w-full h-[calc(100%-52px)]">
                             <div className="border h-full flex items-center justify-center">
-                                {imageBase64 ? (
+                                {
+                                    isLoading ?
+                                    <Loader /> :
                                     <img src={imageBase64} alt="Generated Image" className="max-h-full max-w-full" />
-                                ) : (
-                                    <Loader />
-                                )}
+                                }
                             </div>
                         </div>
                         <button
