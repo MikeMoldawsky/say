bf338cceb9e09679e595f69c620dd55bdc79b9a4
Mike Moldawsky <mike.moldawsky@gmail.com>
2023-04-01 12:09:35 +0300

    try catch on EP

--- say/src/pages/api/bots/[botId]/index.ts
+++ say/src/pages/api/bots/[botId]/index.ts
@@ -6,63 +6,44 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ): Promise<void> {
-	const userId = getUserIdFromHeader(req.headers);
-	if(!userId){
-		res.status(401).json({ message: 'Unauthorized' });
-		return;
-	}
-	const { botId } = req.query;
-	if (req.method === 'GET') {
-		try {
-			const bot = await getBot(botId as string);
-			if (bot) {
+	try {
+		const userId = getUserIdFromHeader(req.headers);
+		if (!userId) {
+			res.status(401).json({message: 'Unauthorized'});
+			return;
+		}
+		const {botId} = req.query;
+		switch (req.method) {
+			case 'GET':
+				const bot = await getBot(botId as string);
+				if (!bot) {
+					res.status(404).json({message: `Bot with user ID ${userId} and bot ID ${botId} not found`});
+					return;
+				}
 				res.status(200).json(bot);
-			} else {
-				res.status(404).json({ message: `Bot with user ID ${userId} and bot ID ${botId} not found` });
-			}
-		} catch (error) {
-			if (error instanceof Error) {
-				res.status(500).json({ message: 'Error fetching bot', error: error.message });
-			} else {
-				res.status(500).json({ message: 'Error fetching bot', error: String(error) });
-			}
+				break;
+			case 'POST':
+				const createBotReq = req.body;
+				const createdBot = await addBot(userId, createBotReq);
+				res.status(201).json(createdBot);
+				break;
+			case 'PUT':
+				const updateBotReq = req.body;
+				await updateBot(updateBotReq);
+				res.status(200).json({message: 'Bot updated successfully'});
+				break;
+			case 'DELETE':
+				await deleteBot(userId, botId as string);
+				res.status(200).json({message: 'Bot deleted successfully'});
+				break;
+			default:
+				res.status(405).json({message: 'Method not allowed'});
 		}
-	} else if (req.method === 'POST') {
-		try {
-			const createBotReq = req.body;
-			const createdBot = await addBot(userId, createBotReq);
-			res.status(201).json(createdBot);
-		} catch (error) {
-			if (error instanceof Error) {
-				res.status(500).json({ message: 'Error creating bot', error: error.message });
-			} else {
-				res.status(500).json({ message: 'Error creating bot', error: String(error) });
-			}
+	} catch (error) {
+		if (error instanceof Error) {
+			res.status(500).json({message: 'Error at bot endpoint', error: error.message});
+		} else {
+			res.status(500).json({message: 'Error at bot endpoint', error: String(error)});
 		}
-	}  else if (req.method === 'PUT') {
-		try {
-			const updateBotReq = req.body;
-			await updateBot(updateBotReq);
-			res.status(200).json({ message: 'Bot updated successfully' });
-		} catch (error) {
-			if (error instanceof Error) {
-				res.status(500).json({ message: 'Error updating bot', error: error.message });
-			} else {
-				res.status(500).json({ message: 'Error updating bot', error: String(error) });
-			}
-		}
-	} else if (req.method === 'DELETE') {
-		try {
-			await deleteBot(userId, botId as string);
-			res.status(200).json({ message: 'Bot deleted successfully' });
-		} catch (error) {
-			if (error instanceof Error) {
-				res.status(500).json({ message: 'Error deleting bot', error: error.message });
-			} else {
-				res.status(500).json({ message: 'Error deleting bot', error: String(error) });
-			}
-		}
-	} else {
-		res.status(405).json({ message: 'Method not allowed' });
 	}
 }
